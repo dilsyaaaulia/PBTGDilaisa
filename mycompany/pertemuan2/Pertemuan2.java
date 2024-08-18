14d76830d1b1167629c3f59cffc7a6ab8fb9f9b6
Dilaisa Aulia <dilsyaaaull@gmail.com>
2024-08-18 16:53:10 +0700

    Add files via upload

---
+++ mycompany/pertemuan2/Pertemuan2.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan2;
+
+/**
+ *
+ * @author DILAISA
+ */
+public class Pertemuan2 {
+    
+public static void main(String[] args){
+    int a=50;
+    float b=a;
+    System.out.println(a);
+    System.out.println(b);
+}}

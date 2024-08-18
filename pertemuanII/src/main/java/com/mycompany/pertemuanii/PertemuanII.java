88cad0ddfe47b03c820990704593bdfe4bd11e6b
Dilaisa Aulia <dilsyaaaull@gmail.com>
2024-08-18 16:33:13 +0700

    Add files via upload

---
+++ pertemuanII/src/main/java/com/mycompany/pertemuanii/PertemuanII.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuanii;
+
+/**
+ *
+ * @author DILAISA
+ */
+public class PertemuanII {
+
+    public static String MakanandanMinumanFavorit;
+     
+    
+     
+     public static void makanandanminuman(){
+         
+         String MakananFavorit = "Ayam Geprek";
+         String MinumanFavorit = "Jus Jambu";
+         
+         
+         System.out.println("Makanan Favorit saya" + ": Ayam Geprek");
+         // Memanggil Variabel Makanan Favorit saya
+         System.out.println("Minuman Favorit saya" + ": Jus Jambu"
+         +" // Makanan Favorit saya" +":Ayam Geprek");
+         // Memanggil Variabel Minuman Favorit saya
+       
+     }
+     
+     
+      
+     public static void main(String[] args){
+         
+         PertemuanII latihan = new PertemuanII();
+         
+         latihan.makanandanminuman();
+     }
+}
+    
+    
+

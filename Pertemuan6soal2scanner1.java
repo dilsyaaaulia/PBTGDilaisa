d00c6099477c783a29eae272104267a1fb1a2525
Dilaisa Aulia <dilsyaaaull@gmail.com>
2024-08-28 04:49:37 -0700

    Add files via upload
    tugas pertemuan6 soal nomor 2

---
+++ Pertemuan6soal2scanner1.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan6soal2scanner1;
+
+/**
+ *
+ * @author DILAISA
+ */
+public class Pertemuan6soal2scanner1 {
+
+    public static void main(String[] args) {
+        int NIM = 1111;
+        int Nilai =85;
+        String Nama ="Agustav";
+        
+        if (NIM>=1111){
+            System.out.println("grade A");
+            System.out.println("Keterangan : Lulus");
+        }
+        
+        else if (NIM>=1111 &&  Nilai<85){
+            System.out.println("grade A");
+        }
+    }
+}
---
+++ Pertemuan6soal2scanner2.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan6soal2scanner2;
+
+/**
+ *
+ * @author DILAISA
+ */
+public class Pertemuan6soal2scanner2 {
+
+    public static void main(String[] args) {
+        int NIM= 1112;
+        int Nilai = 75;
+        String Nama= "Indah";
+        
+        if (NIM>=1112){
+            System.out.println("grade B");
+            System.out.println("Keterangan :lulus");
+        }
+    }
+}

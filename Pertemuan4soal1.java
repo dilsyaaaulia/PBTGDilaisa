88f1f224ffab7220ecf8fce7ad69d74843991191
Dilaisa Aulia <dilsyaaaull@gmail.com>
2024-08-27 21:07:15 -0700

    Add files via upload
    Pertemuan4

---
+++ Pertemuan4soal1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan4soal1;
+
+/**
+ *
+ * @author PC RPL - R1
+ */
+public class Pertemuan4soal1 {
+
+    public static void main(String[] args) {
+        int angka1 = 10;
+        int angka2 = 3;
+        
+        System.out.println("A + B:" + (angka1 + angka2));
+        System.out.println("A - B:" + (angka1 - angka2));
+        System.out.println("A * B:" + (angka1 * angka2));
+        System.out.println("A / B:" + (angka1 / angka2));
+    }
+}

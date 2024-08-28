62fe7bd11db064a65db140bbd2812f97a6a14f45
Dilaisa Aulia <dilsyaaaull@gmail.com>
2024-08-28 05:55:01 -0700

    Add files via upload
    tugas pertemuan7

---
+++ Pertemuan7soal2.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan7soal2;
+
+/**
+ *
+ * @author DILAISA
+ */
+public class Pertemuan7soal2 {
+
+    public static void main(String[] args) {
+        
+        
+        int NIM = 1111111;
+        String Nama="Agustav";
+        int Tahun = 3;
+        char PILIHPEMINATAN = 'M';
+        
+        switch (Tahun){
+            case 1:
+                System.out.println("PEMOGRAMAN MOBILE, PEMROGRAMAN JAVA 2");
+                break;
+            case 2:
+                System.out.println("PEMOGRAMAN MOBILE, PEMOGRAMAN JAVA 2");
+                break;
+        }
+}
+}

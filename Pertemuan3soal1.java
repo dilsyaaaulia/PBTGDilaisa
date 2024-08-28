88953582e072bbb68c51d0969158e41447aee263
Dilaisa Aulia <dilsyaaaull@gmail.com>
2024-08-27 20:50:01 -0700

    Add files via upload
    Pertemuan3

---
+++ Pertemuan3soal1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan3soal1;
+
+/**
+ *
+ * @author DILAISA
+ */
+public class Pertemuan3soal1 {
+
+    public static void main(String[] args) {
+        
+     int   number = 100;
+     float   Var2= 8.99f;
+     double  Var3= 'A';
+     boolean  Var4= false;
+     String   Var5= "Hello Word!";
+    }
+}

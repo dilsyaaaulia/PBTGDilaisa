aae8c379010e06c87be573d9eddd0af656dd0dbb
Dilaisa Aulia <dilsyaaaull@gmail.com>
2024-08-28 01:27:08 -0700

    Add files via upload
    sxqsx

---
+++ Pertemuan6Scanner3.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan6scanner3;
+
+/**
+ *
+ * @author PC RPL - R1
+ */
+public class Pertemuan6Scanner3 {
+
+    public static void main(String[] args) {
+        int NIM = 1113;
+        int Nilai = 65;
+        String Nama = "Rendi";
+        
+        if (NIM>=1113){
+            System.out.println("grade C");
+        }
+        else if (NIM>=1113 && Nilai<65){
+            System.out.println("grade C");
+        }
+    }
+}
---
+++ Pertemuan6soal1scanner2.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan6soal1scanner2;
+
+/**
+ *
+ * @author PC RPL - R1
+ */
+public class Pertemuan6soal1scanner2 {
+
+    public static void main(String[] args) {
+        int NIM = 1112;
+        int Nilai = 75;
+        String Nama = "Indah";
+        
+        if (NIM>=1112){
+            System.out.println("grade B");
+        }
+        
+        else if (NIM>=1112 && Nilai<75){
+            System.out.println("grade B");
+        }
+    }
+}

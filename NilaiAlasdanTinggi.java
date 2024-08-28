5b85133b1ea84a597f22256e392204db498aa840
Dilaisa Aulia <dilsyaaaull@gmail.com>
2024-08-27 21:45:13 -0700

    Add files via upload
    nilai alas yang tinggi itu adalah pertemuan5 soal nomor2

---
+++ NilaiAlasdanTinggi.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.nilaialasdantinggi;
+
+/**
+ *
+ * @author PC RPL - R1
+ */
+public class NilaiAlasdanTinggi {
+
+    public static void main(String[] args) {
+        
+        double alas = 17; double tinggi= 11;
+        double luas = (alas*tinggi)/2;
+        
+        System.out.println("NilaiAlasdanTinggi:" + luas);
+        
+
+    }
+}
---
+++ Pertemuan5soal1.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan5soal1;
+
+/**
+ *
+ * @author DILAISA
+ */
+public class Pertemuan5soal1 {
+
+    public static void main(String[] args) {
+        
+        String NamaKaryawan = "iyan";
+        String Alamat = "Ampenan";
+        String Usia = "20 Tahun";
+        String Gaji = "Rp 10000000";
+     
+        
+        System.out.println("Nama Karyawan:" + NamaKaryawan);
+        System.out.println("Alamat:" + Alamat);
+        System.out.println("Usia:" + Usia);
+        System.out.println("Gaji:" + Gaji);
+                
+    }
+}
